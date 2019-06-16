b53c51fbbcf4bd19c024812e7ea5350bd4cfd639
Tamilselvan T <tamilselvant@Tamilselvans-MacBook-Pro.local>
2019-06-17 01:14:05 +0530

    over all completed

--- src/views/careers.vue
+++ src/views/careers.vue
@@ -1,5 +1,39 @@
 <template>
-  <div class="about">
-    <h1>This is an career page</h1>
-  </div>
+    <div class="container-fluid">
+        <b-img :src="teamImage"/>
+        <div class="container">
+          <h2 class="pt-3 pb-3">Careers</h2>
+          <p>Balubaid offers a wide variety of career opportunities across a spectrum of exciting businesses ranging from automotive and real estate to manufacturing and services. We welcome talented individuals to join our team, and we provide a warm, family oriented culture where collegiality, achievement, and continuous development are highly valued. As a pillar of the Saudi business community, Balubaid is committed to offering a range of rewarding career opportunities for qualified Saudi candidates and contributing to the human capital development of the Kingdom.
+
+</p>
+          <div class="row pb-5 pt-5 mb-5 bg-light">
+              <div class="col-sm-12 pb-3 pt-3">
+                <h3 class="text-center pb-3 pt-3">Discover and apply for open positions at Balubaid Group!</h3>
+                <b-button class="d-flex justify-content-center marign-center" variant="outline-secondary">APPLY NOW</b-button>
+              </div>
+            </div>
+        </div>
+    </div>
+    
 </template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      teamImage: require('./../assets/images/careers-header.jpg')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+img {
+   width: 100%;
+}
+.marign-center {
+  margin: 0 auto;
+}
+</style>
+
+
--- src/views/contact-us.vue
+++ src/views/contact-us.vue
@@ -1,5 +1,78 @@
 <template>
-  <div class="about">
-    <h1>This is an contact us page</h1>
-  </div>
+  <div class="container">
+            <b-form-group method="post">
+                <h2 class="pt-3 pb-3">Contact Us</h2>
+               <div class="row  pt-5 pb-5 mt-5 mb-5">
+                    
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <b-form-textarea name="txtMsg" class="form-control" placeholder="Your Message *" style="width: 100%; height: 150px;"></b-form-textarea>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <b-form-input type="text" name="txtName" class="form-control" placeholder="Your Name *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <b-form-input type="text" name="txtEmail" class="form-control" placeholder="Your Email *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <b-form-input type="text" name="txtPhone" class="form-control" placeholder="Your Phone Number *" value="" />
+                        </div>
+                        <div class="form-group pt-5 pb-5">
+                          <b-button variant="outline-primary">Send Message</b-button>
+                        </div>
+                    </div>
+                    
+                </div>
+            </b-form-group>
+</div>
 </template>
+
+<style lang="scss">
+.contact-form .form-control{
+    border-radius:1rem;
+}
+.contact-image{
+    text-align: center;
+}
+.contact-image img{
+    border-radius: 6rem;
+    width: 11%;
+    margin-top: -3%;
+    transform: rotate(29deg);
+}
+.contact-form form{
+    padding: 14%;
+}
+.contact-form form .row{
+    margin-bottom: -7%;
+}
+.contact-form h3{
+    margin-bottom: 8%;
+    margin-top: -10%;
+    text-align: center;
+    color: #0062cc;
+}
+.contact-form .btnContact {
+    width: 50%;
+    border: none;
+    border-radius: 1rem;
+    padding: 1.5%;
+    background: #dc3545;
+    font-weight: 600;
+    color: #fff;
+    cursor: pointer;
+}
+.btnContactSubmit
+{
+    width: 50%;
+    border-radius: 1rem;
+    padding: 1.5%;
+    color: #fff;
+    background-color: #0062cc;
+    border: none;
+    cursor: pointer;
+}
+</style>
+
